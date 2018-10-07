36f8996512e8061ed4458121730074075ea8f55c
Connor Skipton <cskipton@g2planet.com>
2018-10-07 11:19:17 -0500

    Updating php docs in QueryController, NOTE: this is outdated controller needs to be removed

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class QueryController extends Controller {
 
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return String Template
+     */
     public function search(Request $request)
     {
         // Gets the query string from our form submission
