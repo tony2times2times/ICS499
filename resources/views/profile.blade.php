9a58b901fda3d082b07edbf95fb71f9cb3d45332
Connor Skipton <cskipton@g2planet.com>
2018-10-07 11:18:21 -0500

    Updating profile Controller for added fields, and missing authentication

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+    {{--page specific css--}}
+    <link href="{{ asset('/css/profileController.css') }}" rel="stylesheet">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -8,11 +10,17 @@
                 <div class="panel-body">
                     {!!Form::open(['action' => ['ProfileController@update', $user->id],'method' => 'POST'])!!}
                     {{Form::bsText('name',$user->name,['placeholder' => 'Name'])}}
-                    {{Form::bsText('gender',$user->gender,['placeholder' => 'Gender'])}}
-                    {{Form::bsText('height',$user->height,['placeholder' => 'Height'])}}
-                    {{Form::bsText('weight',$user->weight,['placeholder' => 'Weight'])}}
                     {{Form::bsText('email',$user->email,['placeholder' => 'Email'])}}
-                    {{Form::bsText('calories',$user->calorie_per_day,['placeholder' => 'Calorie Intake Per Day'])}}
+                    {{Form::bsText('gender',$user->gender,['placeholder' => 'Gender'])}}
+                    {{Form::label('age','Age')}}
+                    {{Form::number('age',$user->age,['placeholder' => 'Age'])}}
+                    <br>
+                    {{Form::label('height','Height Inches')}}
+                    {{Form::number('height',$user->height,['placeholder' => 'Height Inches'])}}
+                    <br>
+                    {{Form::label('weight','Weight Lbs')}}
+                    {{Form::number('weight',$user->weight,['placeholder' => 'Weight Lbs'])}}
+                    <br>
                     {{Form::hidden('_method', 'PUT')}}
                     {{Form::bsSubmit('submit')}}
                     {!! Form::close() !!}
