90b19f532855ce57b1fed0e1d029186d06baa066
Tony Peraza <tony.peraz86@gmail.com>
2018-11-04 22:38:57 -0600

    added hyper links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,9 +29,9 @@
                 <br>
                 <br>
                 <p>
-                  The user’s resting metabolic rate(RMR) is first calculated using the Mifflin-St Jeor equation which is the most accurate estimate of the number of calories a person’s body metabolizes in a day. Once the RMR is determined, calories are either subtracted
+                  The user’s resting metabolic rate(RMR) is first calculated using the <a href="http://healthhappinesslongevity.com/ANewPredictiveEquationForRestingEnergyExpenditureInHealthyIndividuals.pdf">Mifflin-St Jeor equation</a> which is the most accurate estimate of the number of calories a person’s body metabolizes in a day. Once the RMR is determined, calories are either subtracted
                   from the RMR to create a caloric deficit to lose weight or added to create a calorie surplus to gain weight based on the user’s goal. The number of calories that is subtracted or added depends on how many pounds per week the user plans
-                  to lose or gain. A new predictive equation for resting energy expenditure in healthy individuals: http://healthhappinesslongevity.com/ANewPredictiveEquationForRestingEnergyExpenditureInHealthyIndividuals.pdf
+                  to lose or gain.
                 </p>
                 <div style="clear:both"></div>
                 </div>
@@ -47,8 +47,8 @@
                 <br>
                 <br>
                 <p>
-                  How is calorie consumption related to weight loss or gain? A study conducted by Max Wishnofsky for The American Journal of Clinical Nutrition determined that 3,500 is the caloric value of one pound of body weight. Therefore, if you were to
-                  consume 3,500 less calories than your RMR over a period of one week, you would lose one pound of body weight a week. Caloric Equivalents of Gained or Lost Weight: https://pdfs.semanticscholar.org/4cbc/8e61bd345d4a76a766cecc05e44ecaa13d13.pdf
+                  A <a href="https://pdfs.semanticscholar.org/4cbc/8e61bd345d4a76a766cecc05e44ecaa13d13.pdf">study conducted by Max Wishnofsky</a> for The American Journal of Clinical Nutrition determined that 3,500 is the caloric value of one pound of body weight. Therefore, if you were to
+                  consume 3,500 less calories than your RMR over a period of one week, you would lose one pound of body weight a week.
                 <div style="clear:both"></div>
               </div>
             </div>
@@ -62,9 +62,8 @@
                 <br>
                 <br>
                 <p>
-                  The Centers for Disease Control and Prevention recommend the following: 2 hours and 30 minutes (150 minutes) of moderate-intensity aerobic activity (i.e., brisk walking) and muscle-strengthening activities on 2 or more days a week that work
-                  all major muscle groups (legs, hips, back, abdomen, chest, shoulders, and arms). Further detail regarding exercise recommendations can be found on the CDC website: https://www.cdc.gov/physicalactivity/basics/adults/index.htm
-              </div>
+                  <a href="https://www.cdc.gov/physicalactivity/basics/adults/index.htm">The Centers for Disease Control and Prevention recommends</a> the following: 2 hours and 30 minutes (150 minutes) of moderate-intensity aerobic activity (i.e., brisk walking) and muscle-strengthening activities on 2 or more days a week that work
+                  all major muscle groups (legs, hips, back, abdomen, chest, shoulders, and arms).
             </div>
           </div>
         </div>
